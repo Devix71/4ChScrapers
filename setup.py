9efb316ea924106cf713b573c30c299b63fa9e9b
Devix71 <60926503+Devix71@users.noreply.github.com>
2020-08-28 14:21:17 +0300

    Create setup.py
    added a setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='4ChanDownloader',
+      version='0.3',
+      packages='4ch_downloader',
+      package_dir={'':'4CH'},
+      url='https://github.com/Devix71/4ChanDownloader',
+      license='GNU General Public License v3.0',
+      author_email='nitronitro60@yahoo.com',
+      description='thread and board downloader for 4chan',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Anyone',
+          'Topic :: Web Download :: Website Scraping',
+          'License :: GNU General Public License v3.0',
+          'Programming Language :: Python :: 3.8'
+      ]
+      )
